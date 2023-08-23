702a5d6c33c35c7a5f3cfd1c9a2059bcd9e25448
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-08-23 15:25:47 -0300

    Entendendo variáveis e Strings em Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
     println("Olá, Mundo!")
+
+    //Variável mutável
+    var nome = "Matheus"
+    println(nome)
+
+    //Variável imutável
+    val nome2 = "Maria"
+
+    //Formatação de Strings (Interpolação)
+    println("Olá: $nome e $nome2")
+
+    //Concatenação de Strings
+    var str = "lkasjdkjsodasçk jklsd" +
+            "askjj skj os sdlsdj asda" +
+            "alskjk kk jsdkja a sdjkr" +
+            "jhsdkhkasd"
+
 }
