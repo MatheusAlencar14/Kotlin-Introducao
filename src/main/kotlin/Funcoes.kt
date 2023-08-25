4f7ad38199acf44b2c79e36771041c626011235d
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-08-25 10:35:05 -0300

    Declarando Funções

---
+++ src/main/kotlin/Funcoes.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    helloWord()
+    
+    println("A soma de 17 + 10 é: ${soma(17, 10)}")
+}
+
+fun soma(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun helloWord() {
+    println("Olá, Mundo!")
+}
