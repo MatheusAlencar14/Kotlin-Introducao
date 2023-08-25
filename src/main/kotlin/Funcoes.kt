0249f0f565a4ffedff7956ac64dab196a255d65d
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-08-25 10:56:16 -0300

    Funções simplificadas em uma linha

--- src/main/kotlin/Funcoes.kt
+++ src/main/kotlin/Funcoes.kt
@@ -1,14 +1,18 @@
 fun main() {
 
     helloWord()
-    
+
     println("A soma de 17 + 10 é: ${soma(17, 10)}")
+    println("A divisão é: ${div1(15.5f, 3.3f)}")
+    println("A divisão é: ${div2(9.9f, 3.0f)}")
 }
 
-fun soma(a: Int, b: Int): Int {
-    return a + b
+fun soma(a: Int, b: Int) = a + b
+
+fun helloWord() = println("Olá, Mundo!")
+
+fun div1(c: Float, d: Float): Float {
+    return c / d
 }
 
-fun helloWord() {
-    println("Olá, Mundo!")
-}
+fun div2(c: Float, d: Float) = c / d
