ca5a5f9b69f46e3ad079f2fe3160a3e1d8fdfe09
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-18 10:41:55 -0300

    Funções Matemáticas (Math)

---
+++ src/main/kotlin/FuncoesMatematicas.kt
@@ -0,0 +1,18 @@
+import kotlin.math.*
+
+fun main() {
+
+    //Verificar o maior entre dois números
+    println(max(10, 17))
+    //Verificar o menor entre dois números
+    println(min(10, 17))
+    //Verificar raiz quadrada
+    println(sqrt(25f))
+    //Numero de PI
+    println(PI)
+    //Numero de Euler
+    println(E)
+    //Arredondar o valor de um número
+    println(round(8.54))
+
+}
