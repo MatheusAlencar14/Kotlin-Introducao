6daac6bd0512c87cf8ba70e99ee1d2a708b6992e
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-09-22 11:35:00 -0300

    Leitura de Dados

---
+++ src/main/kotlin/LeituraDeDados.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var idade = readln()
+    
+    if (idade < 18.toString()) {
+        println("Menor de idade")
+    } else {
+        println("Maior de idade")
+    }
+    println(idade)
+}
