57f64cf1e4b764a3d347f84718fc9b18dacaa9ab
MatheusAlencar14 <matheusalencar14@hotmail.com>
2023-10-03 15:13:50 -0300

    Tratamento de Exceções - Exception

---
+++ src/main/kotlin/Excecoes.kt
@@ -0,0 +1,15 @@
+fun main() {
+    try {
+        val str: String? = null
+        println(str!!.length) // NullPointerException
+        println(12 / 0) // ArithmeticException
+    } catch (e: ArithmeticException) {
+        println("Divisão por zero!")
+    } catch (e: NullPointerException) {
+        println("NullPointer!!")
+    } catch (e: Exception) {
+        println("Tivemos um problema.")
+    } finally {
+        println("Finalmente!")
+    }
+}
